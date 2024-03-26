ba9a26911a88c4eb88d05cdaeecbeec142550efb
koshenkovv <tim.koshenkov@gmail.com>
2024-03-26 20:08:12 +0300

    comments + images

--- assets/scripts/script_main.js
+++ assets/scripts/script_main.js
@@ -58,3 +58,28 @@ function switchComments() {
     console.error("Элемент с классом appeal-comments не найден.");
   }
 }
+
+function switchImageShow() {
+  var element = document.querySelector(".descktop-image-show");
+  if (element.style.display === "none" || element.style.display === "") {
+    element.style.display = "flex";
+  } else {
+    element.style.display = "none";
+  }
+}
+
+function showImage(event) {
+  var imgShowcase = document.getElementById("imageShowcase");
+
+  // Исправлено: Извлечение style.backgroundImage напрямую из event.target может не сработать, как ожидается.
+  // Нужно проверить, что event.target действительно содержит background-image.
+  var imageUrl = getComputedStyle(event.target).backgroundImage; // Получаем полный стиль элемента, включая background-image.
+
+  // Очищаем URL от "url(" и ")" с помощью регулярного выражения, а также от возможных кавычек.
+  var cleanUrl = imageUrl.replace(/(url\(|\)|"|')/g, "");
+
+  // Устанавливаем извлеченный URL в качестве src для img.
+  imgShowcase.src = cleanUrl;
+
+  switchImageShow();
+}
